82658b9c0790c8a8839c96513fc9c48020b7712f
Vladimir Ilyin <ilyinvp@yandex.ru>
2024-04-16 00:18:41 +0300

    add dynamic main page

--- README.md
+++ README.md
@@ -6,8 +6,7 @@ This is yet another accounting app that lets you manage your finances. The name 
 
 ## TODO
 
-- [ ] Make it deploy
-- [ ] Scaffold basic UI with mock data
-- [ ] Actually set up a db
+- [x] Make it deploy
+- [x] Actually set up a db
 - [ ] Attach database to UI
 - [ ] Routing
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,94 +1,22 @@
-"use client";
+import { db } from "~/server/db";
 
-import { useBackButton, useInitData, useMainButton } from "@tma.js/sdk-react";
-import { useEffect, useMemo, useState } from "react";
+export const dynamic = "force-dynamic";
 
-function MainButtonTest() {
-  const mainButton = useMainButton();
-  const backButton = useBackButton();
-
-  const [count, setCount] = useState(0);
-
-  useEffect(() => {
-    const onMainButtonClick = () => setCount((prevCount) => prevCount + 1);
-    const onBackButtonClick = () => setCount((prevCount) => prevCount - 1);
-
-    mainButton.enable().show();
-    mainButton.on("click", onMainButtonClick);
-    backButton.on("click", onBackButtonClick);
-
-    return () => {
-      mainButton.off("click", onMainButtonClick);
-      mainButton.hide();
-      backButton.off("click", onBackButtonClick);
-    };
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  useEffect(() => {
-    mainButton.setText(`Count is ${count}`);
-  }, [mainButton, count]);
-
-  useEffect(() => {
-    if (count === 0) {
-      backButton.hide();
-      return;
-    }
-    backButton.show();
-  }, [backButton, count]);
-
-  return null;
-}
-
-/**
- * Displays current application init data.
- */
-function InitData() {
-  const initData = useInitData();
-
-  const initDataJson = useMemo(() => {
-    if (!initData) {
-      return "Init data is empty.";
-    }
-    const {
-      authDate,
-      chat,
-      hash,
-      canSendAfter,
-      queryId,
-      receiver,
-      user,
-      startParam,
-    } = initData;
-
-    return JSON.stringify(
-      {
-        authDate,
-        chat,
-        hash,
-        canSendAfter,
-        queryId,
-        receiver,
-        user,
-        startParam,
-      },
-      null,
-      " ",
-    );
-  }, [initData]);
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
 
   return (
-    <pre>
-      <code>{initDataJson}</code>
-    </pre>
-  );
-}
-
-export default function Home() {
-  return (
-    <>
-      <MainButtonTest />
-      <InitData />
-    </>
+    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+      <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
+        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+          Create <span className="text-[hsl(280,100%,70%)]">T3</span> App
+        </h1>
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8">
+          {posts.map((post) => (
+            <div key={post.id}>{post.name}</div>
+          ))}
+        </div>
+      </div>
+    </main>
   );
 }
