7f90c4372dd0803829efe5cbf145f35703d58d2f
Vladimir Ilyin <ilyinvp@yandex.ru>
2024-04-18 20:11:34 +0300

    test qr scanner

--- app/tma/page.tsx
+++ app/tma/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import { Button } from "~/shared/ui/button";
 import WelUser from "./welUser";
 import BackButton from "~/shared/ui/back-button";
+import QrScanner from "./qr";
 
 export default async function HomePage() {
   return (
@@ -9,9 +10,13 @@ export default async function HomePage() {
       <div className="flex min-h-screen flex-col items-center justify-center">
         <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
           <WelUser />
-          <Button variant="outline">
-            <Link href="/tma/incomes">Incomes</Link>
-          </Button>
+          <div className="container flex flex-wrap gap-2">
+            <Button variant="outline">
+              <Link href="/tma/incomes">Incomes</Link>
+            </Button>
+            <QrScanner />
+            <Button variant="outline">Hide sensitive</Button>
+          </div>
         </div>
       </div>
       <BackButton hide />
