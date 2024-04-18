7e9cc133b0c855a66df13e05dc408ffc52cae607
Vladimir Ilyin <ilyinvp@yandex.ru>
2024-04-18 22:42:22 +0300

    fix adaptivity for init page buttons

--- src/widgets/logged-off.tsx
+++ src/widgets/logged-off.tsx
@@ -6,7 +6,7 @@ import TelegramButton from "~/shared/ui/telegram-button";
 
 export default function LoggedOffWidget() {
   return (
-    <Card className="mx-auto my-20 flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-12 lg:py-20">
+    <Card className="mx-auto my-20 flex max-w-[980px] flex-col items-center gap-2 px-4 py-8 md:py-12 md:pb-12 lg:py-20">
       <h1 className="text-center text-3xl font-bold leading-tight tracking-tighter md:text-6xl lg:leading-[1.1]">
         Manage your finances
       </h1>
@@ -14,11 +14,11 @@ export default function LoggedOffWidget() {
         Accountant offers a fast, efficient and beautifully-designed solution to
         keep track of your savings. Integrated with Telegram.
       </span>
-      <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-10">
+      <div className="flex w-full flex-col items-center justify-center gap-4 py-4 md:flex-row md:pb-10">
         <TelegramButton botUsername={env.BOT_USERNAME} />
         <Link href={env.MINI_APP_URL} target="_blank">
           <Button
-            className="text-md h-10 w-[200px] rounded-xl pb-[13px] pt-[11px]"
+            className="text-md h-10 w-[225px] rounded-xl pb-[13px] pt-[11px] md:w-[200px]"
             variant="secondary"
           >
             Open Mini App
