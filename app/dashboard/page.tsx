dfce96b7b462083f6c54530468b8382d334cab73
Vladimir Ilyin <ilyinvp@yandex.ru>
2024-04-18 23:08:14 +0300

    add 404, start adding dashboard

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import { authOptions } from "app/api/auth/[...nextauth]/route";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
+import { BentoGrid, BentoGridItem } from "~/shared/ui/bento-grid";
 import { Card, CardHeader, CardTitle, CardContent } from "~/shared/ui/card";
 
 export default async function DashboardPage() {
@@ -10,16 +11,54 @@ export default async function DashboardPage() {
     redirect("/");
   }
 
+  const data = { id: session.user.id, name: session.user.name };
+
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Session Data</CardTitle>
-        <CardContent>
-          <pre>
-            <blockquote>{JSON.stringify(session.user, null, " ")}</blockquote>
-          </pre>
-        </CardContent>
-      </CardHeader>
-    </Card>
+    <BentoGrid className="mx-auto max-w-4xl md:auto-rows-[20rem]">
+      <BentoGridItem
+        className="md:col-span-2"
+        header={
+          <Card>
+            <CardHeader>
+              <CardTitle>Session Data</CardTitle>
+              <CardContent>
+                <pre>
+                  <blockquote>{JSON.stringify(data, null, " ")}</blockquote>
+                </pre>
+              </CardContent>
+            </CardHeader>
+          </Card>
+        }
+      />
+      <BentoGridItem
+        className="md:col-span-1"
+        header={
+          <Card>
+            <CardHeader>
+              <CardTitle>Session Data</CardTitle>
+              <CardContent>
+                <pre>
+                  <blockquote>{JSON.stringify(data, null, " ")}</blockquote>
+                </pre>
+              </CardContent>
+            </CardHeader>
+          </Card>
+        }
+      />
+      <BentoGridItem
+        header={
+          <Card>
+            <CardHeader>
+              <CardTitle>Session Data</CardTitle>
+              <CardContent>
+                <pre>
+                  <blockquote>{JSON.stringify(data, null, " ")}</blockquote>
+                </pre>
+              </CardContent>
+            </CardHeader>
+          </Card>
+        }
+      />
+    </BentoGrid>
   );
 }
