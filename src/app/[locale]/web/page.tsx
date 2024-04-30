a42d4b695f9788419875142453ba0c6264cd2309
Vladimir Ilyin <ilyinvp@yandex.ru>
2024-04-30 20:10:39 +0300

    remove bento grid

--- src/app/[locale]/web/page.tsx
+++ src/app/[locale]/web/page.tsx
@@ -1,55 +1,29 @@
 import { redirect } from "next/navigation";
-import { BentoGrid, BentoGridItem } from "~/components/bento-grid/bento-grid";
-
 import OperationsCard from "~/widgets/operations/operations-card";
 import BalanceCard from "~/components/balance/balance";
-import { Card, CardContent, CardHeader } from "~/components/card/card";
 import AccountsCard from "~/widgets/accounts/accounts-card";
 import { getSessionUser } from "~/shared/utils/getServerSession";
-import { useTranslations } from "next-intl";
 import OperationsByDayChart from "~/widgets/charts/operation-by-day";
-
-function DummyCard() {
-  const t = useTranslations("web");
-  return (
-    <Card>
-      <CardHeader>{t("dummy")}</CardHeader>
-      <CardContent></CardContent>
-    </Card>
-  );
-}
+import { Button } from "~/components/button/button";
 
 export default async function DashboardPage() {
   const user = await getSessionUser();
   if (!user) redirect("/");
 
   return (
-    <>
-      <BentoGrid>
-        <BentoGridItem colSpan={1}>
-          <DummyCard />
-        </BentoGridItem>
-        <BentoGridItem colSpan={1}>
-          <BalanceCard userId={+user.id} />
-        </BentoGridItem>
-        <BentoGridItem>
-          <OperationsCard userId={+user.id} />
-        </BentoGridItem>
-        <BentoGridItem colSpan={1}>
-          <DummyCard />
-        </BentoGridItem>
-        <BentoGridItem colSpan={1}>
-          <AccountsCard userId={+user.id} />
-        </BentoGridItem>
-        <BentoGridItem colSpan={1}>
-          <DummyCard />
-        </BentoGridItem>
-        <BentoGridItem colSpan={1}>
-          <DummyCard />
-        </BentoGridItem>
-        <BentoGridItem colSpan={2}></BentoGridItem>
-      </BentoGrid>
+    <div className="space-y-4 p-8 pt-6">
+      <header className="flex items-center justify-between space-y-2">
+        <h2 className="text-3xl font-bold tracking-tight">Dashboard</h2>
+        <div className="flex items-center space-x-2">
+          <Button variant="default" disabled>
+            Download
+          </Button>
+        </div>
+      </header>
+      <BalanceCard userId={+user.id} />
+      <OperationsCard userId={+user.id} />
+      <AccountsCard userId={+user.id} />
       <OperationsByDayChart />
-    </>
+    </div>
   );
 }
