499bc0938326a03734f1ac9d57c8eb7ca4e453c7
Vladimir Ilyin <ilyinvp@yandex.ru>
2024-05-07 01:36:17 +0300

    (v2): new types

---
+++ src/server/db/types.ts
@@ -0,0 +1,11 @@
+import {
+  type accounts,
+  type categories,
+  type transactions,
+  type users,
+} from "./schema";
+
+export type User = typeof users.$inferSelect;
+export type Account = typeof accounts.$inferSelect;
+export type Transaction = typeof transactions.$inferSelect;
+export type Category = typeof categories.$inferSelect;
