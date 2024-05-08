988fe0b64c68c5dc76c06be4d27efcca9e8c5de1
Vladimir Ilyin <ilyinvp@yandex.ru>
2024-05-08 06:32:58 +0300

    separate dashboard layout

--- src/app/[locale]/dashboard/@transactions/page.tsx
+++ src/app/[locale]/dashboard/@transactions/page.tsx
@@ -1,18 +1,18 @@
-import { getTransactions } from "@/server/transactions";
+// import { getTransactions } from "@/server/transactions";
 import { NextIntlClientProvider } from "next-intl";
 import { getMessages } from "next-intl/server";
-import { DataTable } from "./data-table";
+// import { DataTable } from "./data-table";
+import MockTable from "./temp";
 
 export default async function TransactionsPage() {
   const messages = await getMessages();
 
-  const transactions = await getTransactions();
+  // const transactions = await getTransactions();
 
   return (
-    <div className="container mx-auto py-10">
-      <NextIntlClientProvider messages={messages}>
-        <DataTable data={transactions} />
-      </NextIntlClientProvider>
-    </div>
+    <NextIntlClientProvider messages={messages}>
+      {/* <DataTable data={transactions} /> */}
+      <MockTable />
+    </NextIntlClientProvider>
   );
 }
--- src/app/[locale]/dashboard/layout.tsx
+++ src/app/[locale]/dashboard/layout.tsx
@@ -5,6 +5,7 @@ import Header from "../_components/header";
 export default function WebLayout(props: {
   children: React.ReactNode;
   transactions: React.ReactNode;
+  details: React.ReactNode;
 }) {
   return (
     <>
@@ -12,8 +13,13 @@ export default function WebLayout(props: {
         <Aside />
         <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
           <Header />
-          {props.children}
-          {props.transactions}
+          <main className="grid flex-1 items-start gap-4 p-4 sm:px-6 sm:py-0 md:gap-8 lg:grid-cols-3 xl:grid-cols-3">
+            <div className="grid auto-rows-max items-start gap-4 md:gap-8 lg:col-span-2">
+              {props.children}
+              {props.transactions}
+            </div>
+            <div>{props.details}</div>
+          </main>
         </div>
       </div>
       <AppCommandDialog />
