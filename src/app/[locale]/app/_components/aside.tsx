d3baed4810c664e2ba940a132560be8b0e27b423
Vladimir Ilyin <ilyinvp@yandex.ru>
2024-05-10 05:44:26 +0300

    add page templates for aside

--- src/app/[locale]/app/_components/aside.tsx
+++ src/app/[locale]/app/_components/aside.tsx
@@ -11,8 +11,8 @@ import {
   LineChart,
   Network,
   Settings,
-  ShoppingCart,
-  Users2,
+  ArrowLeftRight,
+  Wallet,
 } from "lucide-react";
 import Link from "next/link";
 import ThemeDropdown from "./theme-dropdown";
@@ -30,10 +30,12 @@ export default function Aside() {
   return (
     <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
       <nav className="flex flex-col items-center gap-4 px-2 sm:py-4">
-        <div className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base">
-          <LogoIcon className="h-4 w-4 transition-all group-hover:scale-110" />
-          <span className="sr-only">Accountant</span>
-        </div>
+        <Link href="/app/dashboard">
+          <div className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base">
+            <LogoIcon className="h-4 w-4 transition-all group-hover:scale-110" />
+            <span className="sr-only">Accountant</span>
+          </div>
+        </Link>
         <Tooltip>
           <TooltipTrigger asChild>
             <Link
@@ -49,14 +51,26 @@ export default function Aside() {
         <Tooltip>
           <TooltipTrigger asChild>
             <Link
-              href="#"
+              href="/app/transactions"
               className={`${cn("transactions")} flex h-9 w-9 items-center justify-center rounded-lg transition-colors hover:text-foreground md:h-8 md:w-8`}
             >
-              <ShoppingCart className="h-5 w-5" />
+              <ArrowLeftRight className="h-5 w-5" />
               <span className="sr-only">Transactions</span>
             </Link>
           </TooltipTrigger>
           <TooltipContent side="right">Transactions</TooltipContent>
+        </Tooltip>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link
+              href="/app/accounts"
+              className={`${cn("accounts")} flex h-9 w-9 items-center justify-center rounded-lg transition-colors hover:text-foreground md:h-8 md:w-8`}
+            >
+              <Wallet className="h-5 w-5" />
+              <span className="sr-only">Accounts</span>
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent side="right">Accounts</TooltipContent>
         </Tooltip>
         <Tooltip>
           <TooltipTrigger asChild>
@@ -73,20 +87,8 @@ export default function Aside() {
         <Tooltip>
           <TooltipTrigger asChild>
             <Link
-              href="#"
-              className={`${cn("transactions")} flex h-9 w-9 items-center justify-center rounded-lg transition-colors hover:text-foreground md:h-8 md:w-8`}
-            >
-              <Users2 className="h-5 w-5" />
-              <span className="sr-only">Customers</span>
-            </Link>
-          </TooltipTrigger>
-          <TooltipContent side="right">Customers</TooltipContent>
-        </Tooltip>
-        <Tooltip>
-          <TooltipTrigger asChild>
-            <Link
-              href="#"
-              className={`${cn("transactions")} flex h-9 w-9 items-center justify-center rounded-lg transition-colors hover:text-foreground md:h-8 md:w-8`}
+              href="/app/analytics"
+              className={`${cn("analytics")} flex h-9 w-9 items-center justify-center rounded-lg transition-colors hover:text-foreground md:h-8 md:w-8`}
             >
               <LineChart className="h-5 w-5" />
               <span className="sr-only">Analytics</span>
